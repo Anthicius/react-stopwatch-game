4a7943342140e5a71aaa0be27db6145b9ded022b
Scaevitus <andrius.makseckas@gmail.com>
2025-05-25 17:30:21 +0300

    Added a TimerChallenge component that tracks start and stop with Ref

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useState, useRef } from 'react'
+
+const TimerChallenge = ({title, targetTime}) => {
+    const [timerStarted, setTimerStarted] = useState(false)
+    const [timerExpired, setTimerExpirted] = useState(false)
+    const timer = useRef();
+
+
+    const handleStart = () => {
+        timer.current = setTimeout(()=>{
+            setTimerExpirted(true)
+        }, targetTime*1000)
+        setTimerStarted(true)
+    }
+
+
+    const handleStop = () => {
+        clearTimeout(timer.current);
+    }
+
+  return (
+    <section className='challenge'>
+        <h2>{title}</h2>
+        {timerExpired && <p>You lost</p>}
+        <p className='challenge-time'>{targetTime} second{targetTime >1 ? 's':''}</p>
+        <p>
+            <button onClick={timerStarted ? handleStop : handleStart }>
+                {timerStarted? "Stop" : "Start"} Challenge
+            </button>
+        </p>
+        <p className={timerStarted ? 'active' : undefined}>{timerStarted ? 'Time is running...' : 'Timer innactive'}</p>
+    </section>
+  )
+}
+
+export default TimerChallenge
